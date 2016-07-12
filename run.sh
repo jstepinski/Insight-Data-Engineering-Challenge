bfae8183c30d1860b7c0e35d9dd4c53856837eb7
jstepinski <jstep@alumni.stanford.edu>
2016-07-11 23:13:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 gcc -g -O0 -std=c99 -Wall -Wfloat-equal -Wtype-limits -Wpointer-arith -Wlogical-op src/venmoGraphParams.h src/list.h src/list.c src/table.h src/table.c src/main.c -o venGraph
 
-./venGraph venmo_input/venmo_trans.txt venmo_output/output.txt
+./venGraph venmo_input/venmo-trans.txt venmo_output/output.txt
