abe11fbc8ed3e2bceeb848c70778fff623b2c8e7
jstepinski <jstep@alumni.stanford.edu>
2016-07-11 23:08:08 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gcc -g -O0 -std=c99 -Wall -Wfloat-equal -Wtype-limits -Wpointer-arith -Wlogical-op src/venmoGraphParams.h src/list.h src/list.c src/table.h src/table.c src/main.c -o venGraph
+
+./venGraph venmo_input/venmo_trans.txt venmo_output/output.txt
